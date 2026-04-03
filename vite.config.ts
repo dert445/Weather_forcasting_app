cf3fc6a043ee53a8e48706ed002704d694673c4a
dert445 <is25bm001@iitdh.ac.in>
2026-04-03 23:59:23 +0530

    Fixed vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/Weather_forcasting_app/',
+    base: '/weather_forcasting_app/',
     plugins: [react()],
     resolve: {
       alias: {
